1aea1613643ee9b586fa6ee7b4470e88d76e9258
Naglis <77675440+naglissul@users.noreply.github.com>
2024-08-10 12:29:43 +0200

    Cors profiles (#23)
    * Prod cors fix
    
    * Cors profiles
    
    ---------
    
    Co-authored-by: Naglis <naglis.suliokas@gmail.com>

---
+++ src/main/kotlin/lt/skafis/bankas/config/SecurityConfigProd.kt
@@ -0,0 +1,48 @@
+package lt.skafis.bankas.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+@Profile("prod")
+class SecurityConfigProd {
+
+    @Bean
+    fun genericFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http.authorizeHttpRequests { authorize ->
+            authorize
+                .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/index.html", "/error").permitAll()
+                .requestMatchers("/**").permitAll()
+                .anyRequest().denyAll()
+        }
+            .oauth2ResourceServer { oauth2 ->
+                oauth2.jwt(Customizer.withDefaults())
+            }
+            .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults())
+            .cors { it.configurationSource(corsConfigurationSource()) } // This should be changed for DEV env
+            .csrf { it.disable() }
+            .build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): UrlBasedCorsConfigurationSource {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("https://bankas.skafis.lt")
+        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH")
+        config.allowedHeaders = listOf("*")
+        config.allowCredentials = true
+        config.maxAge = 3600L
+        source.registerCorsConfiguration("/**", config)
+        return source
+    }
+}
